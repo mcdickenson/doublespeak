4007b28ecd4adc179b239f732f9624e90b6f830f
Matt Dickenson <mcdickenson@gmail.com>
2013-08-12 10:09:00 -0400

    implement doublespeak for dictionary

--- doublespeak.js
+++ doublespeak.js
@@ -1,3 +1,27 @@
-document.body.innerHTML = document.body.innerHTML.replace(
-	new RegExp("extrajudicial", "g"), "illegal"
-);
+var dictionary = {
+	"extrajudicial": "illegal",
+}
+
+String.prototype.titleize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+function replaceDoubleSpeak(dictionary, content){
+
+	var patterns = [], 
+    patternHash = {},
+    oldkey, key, index = 0,
+    output = [];
+
+	for (key in dictionary) {
+		content = content.replace(new RegExp(key, "g"), dictionary[key]);
+
+		newkey = key.titleize();
+		dictionary[newkey] = dictionary[key].titleize();
+		content = content.replace(new RegExp(newkey, "g"), dictionary[newkey]); 
+  }
+
+  return content;
+}
+
+document.body.innerHTML = replaceDoubleSpeak(dictionary, document.body.innerHTML);
